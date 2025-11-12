5081bcdaee032d96867d88014e476aab85b9ac7d
Sherlock-YH <yh1259891725@gmail.com>
2025-11-12 20:26:01 +0800

    Spring Initializr: Create a Maven Project

---
+++ backend/src/test/java/com/sherlock/expensesplitter/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sherlock.expensesplitter.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
